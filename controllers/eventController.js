91deec22c0ff076c0e0c88edc1d86a986e34b3d9
Andrés Rava <andresravamail@gmail.com>
2022-11-19 23:41:48 -0300

    empecé a implementar rutas para day of the week

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,9 +2,38 @@ const Event = require('./../models/eventModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+const slugify = require('slugify');
+
+// Función para sacar el día de la semana
+
+const days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+  
+const day = (dateEvent) => {
+  console.log("el dateEvent es: " + dateEvent)
+  return days[ dateEvent.getDay() ]; 
+}
+
+const filterByDay = (events, day) => {
+  console.log("Entré a la función");
+  console.log(events);
+  console.log(day);
+  const filtredEvents = new [Event];
+  console.log("creé el array");
+  console.log("Entré a FilterByDay! ")
+  for (let event of events) {
+    console.log("Evento: " + event)
+    if (event.dateTime.day === day)
+      filtredEvents.push(event);
+  }
+  return filtredEvents;
+}
 
 exports.getAllEvents = async (req, res) => {
     try {
+      console.log(req.query);
+      const llave = slugify(req.query.dayOfTheWeek, { lower: true });
+      console.log(llave);
+      
       // EXECUTE QUERY
       const features = new APIFeatures(Event.find(), req.query)
         .filter()
@@ -12,7 +41,12 @@ exports.getAllEvents = async (req, res) => {
         .limitFields()
         .paginate();
       const events = await features.query;
-  
+      if (days.includes(llave) && llave){
+        console.log("Entré al if!...");
+        events = filterByDay(events, llave);
+        console.log("Después...")
+      }
+
       // SEND RESPONSE
       res.status(200).json({
         status: 'success',
