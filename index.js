e36b9d05641fb554f299096e13b24615c02efe46
Andrés Rava <andresravamail@gmail.com>
2023-01-11 15:21:34 -0300

    server.js imported on index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const server = require('./server');
+const server = require('./server');
 // module.exports = server;
 
 const mongoose = require("mongoose");
@@ -43,25 +43,25 @@ app.use("/api/v1/users", userRouter);
 
 module.exports = app;
 
-console.log(process.env.PORT + " Esto");
-const port = process.env.PORT || 3000;
-console.log("Punto 3");
-console.log("port es: " + port);
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
-console.log("pasé!");
+// console.log(process.env.PORT + " Esto");
+// const port = process.env.PORT || 3000;
+// console.log("Punto 3");
+// console.log("port es: " + port);
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
+// console.log("pasé!");
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-console.log("Punto 2");
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!"));
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+// console.log("Punto 2");
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB connection successful!"));
