b3fd801f96bd80882ee35e000319cad51bf21644
Andrés Rava <andresravamail@gmail.com>
2023-01-11 15:59:54 -0300

    fat app, thin index again

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const morgan = require("morgan");
 const compression = require("compression");
 const eventRouter = require("./routes/eventRoutes");
 const userRouter = require("./routes/userRoutes");
-
 const app = express();
 
 dotenv.config({ path: "./config.env" });
@@ -23,27 +22,22 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
-
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   console.log("Hello from the middleware 👋");
   next();
 });
 
 app.use(compression());
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
 // 3) ROUTES
 app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/users", userRouter);
